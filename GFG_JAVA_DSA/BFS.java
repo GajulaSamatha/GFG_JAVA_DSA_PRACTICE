c35f276fd13e1315cdcaf8673d285266cf44f67e
Samatha Gajula <samathagajulaofficial@gmail.com>
2025-08-11 09:22:08 +0000

    DFS completed

--- GFG_JAVA_DSA/BFS.java
+++ GFG_JAVA_DSA/BFS.java
@@ -7,12 +7,14 @@
  */
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Stack;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Stack;
 class BFS{
     public static void BFSTraversal(Scanner sc){
         Map<String, List<String>> graph = Map.of(
@@ -41,8 +43,34 @@ class BFS{
             }
         }
     }
+     public static void DFSTraversal(Scanner sc){
+        Map<String, List<String>> graph = Map.of(
+        "A", List.of("B", "C"),
+        "B", List.of("A", "C", "D", "E"),
+        "C", List.of("A", "B", "E"),
+        "D", List.of("B", "E"),
+        "E", List.of("B", "C", "D")
+        );
+        Stack<String> to_do = new Stack<>();
+        Set<String> visited = new HashSet<>();
+        String current = sc.next();
+        to_do.addElement(current);
+        visited.add(current);
+        while(!to_do.isEmpty()){
+            String currString = to_do.pop();
+            // The to_do.pop() command gives us the next room to check from our stack.
+            // We need to save that room's name in a new variable, maybe we can call it currentRoom, right inside the while loop.
+            System.out.println("our current room is :"+ currString);
+            for(String neighbour: graph.get(currString )){
+                if(!visited.contains(neighbour)){
+                    visited.add(neighbour);
+                    to_do.push(neighbour);
+                }
+            }
+        }
+     } 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        BFSTraversal(sc);
+        DFSTraversal(sc);
     }
 }
